a63c11949f9312d7f6ebb6193051cfca5e926539
carlafber <carla.fueber@educa.jcyl.es>
2024-12-18 09:33:06 +0100

    Delete proyecto_hibernate directory

--- proyecto_hibernate/src/main/java/com/example/proyecto_hibernate/classes/TipoProfesor.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyecto_hibernate.classes;
-
-//Clase que representa los tipos de profesores que existen
-public enum TipoProfesor {
-    jefe_de_estudios,
-    profesor
-}
--- proyecto_hibernate/src/main/java/com/example/proyecto_hibernate/util/Configurable.java
+++
@@ -1,8 +0,0 @@
-package com.example.proyecto_hibernate.util;
-
-//interfaz que define el comportamiento de la configuración de botones
-
-public interface Configurable {
-    //método para configurar botones activándolos o desactivándolos según si se trata de profesor o jefe de estudios
-    void configurarBotones(Boolean estado);
-}
